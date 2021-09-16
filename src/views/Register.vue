32497ac4be31f7edcd63eea515d064c6cc623fa1
AgentProxy <ericjoseph.flores1@gmail.com>
2021-09-16 08:42:39 +0800

    Responsiveness fix for Login and Register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container class="pa-0 fill-height" fluid>
-    <v-row class="fill-height" align="center" justify="center">
+    <v-row class="fill-height" align="center">
       <v-col class="fill-height pa-0 d-none d-md-block" lg="7" md="6" cols="12">
         <WelcomeContainer />
       </v-col>
-      <v-col class="text-center pa-0" lg="5" md="6" cols="12">
+      <v-col class="fill-height text-center pa-0" lg="5" md="6" cols="12">
         <div class="py-5 d-md-none d-flex align-center justify-center">
           <v-img
             alt="U-Reg Logo"
