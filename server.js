a0e8d211fa0cf89417dcd32cdf1e04b2476dd4f7
AgentProxy <ericjoseph.flores1@gmail.com>
2021-09-16 09:06:01 +0800

    Deployment fix for Heroku

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
 
 const app = express();
 
 // Serve files from dist directory
-app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(express.static(__dirname + "/dist/"));
+// Fix for error in retrieving routes when running build files
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
 // Set port config
 const port = process.env.PORT || 8000;
 app.listen(port);
